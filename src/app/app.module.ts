502eae9352bb4a7caa6cff158f9e9943c2e98446
OderoOluoch <oderoluoch@gmail.com>
2021-05-16 19:49:05 +0300

    Added the form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './components/quote/quote.component';
 import { QuoteDetailComponent } from './components/quote-detail/quote-detail.component';
 import { DateCountPipe } from './res/date-count.pipe';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { DateCountPipe } from './res/date-count.pipe';
     QuoteComponent,
     QuoteDetailComponent,
     DateCountPipe,
+    QuoteFormComponent,
   
   ],
   imports: [
