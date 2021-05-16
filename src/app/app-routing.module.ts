2dfafdb9ee70cd3820e9a9c13b4b72eec8045a8b
OderoOluoch <oderoluoch@gmail.com>
2021-05-16 22:03:41 +0300

    Set up routing complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { QuoteComponent } from '../app/components/quote/quote.component';
+import { AboutComponent } from '../app/components/about/about.component';
+const routes: Routes = [
+  {path:'qoutes', component:QuoteComponent},
+  {path:'about', component:AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
