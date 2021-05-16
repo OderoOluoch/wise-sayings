0238e5d9a012b771c1c8f0e8e2cbc325883b75b2
OderoOluoch <oderoluoch@gmail.com>
2021-05-16 20:04:20 +0300

    UPdated detail and single view component

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -4,36 +4,59 @@ import { Quote } from '../../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
 })
 export class QuoteComponent implements OnInit {
-
-  quotes:Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2020,3,14)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2020,3,14)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2020,3,14)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2020,3,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2020,3,14)),
+  quotes: Quote[] = [
+    new Quote(
+      1,
+      'Watch finding Nemo',
+      'Find an online version and watch merlin find his son',
+      new Date(2020, 3, 14)
+    ),
+    new Quote(
+      2,
+      'Buy Cookies',
+      'I have to buy cookies for the parrot',
+      new Date(2020, 3, 14)
+    ),
+    new Quote(
+      3,
+      'Get new Phone Case',
+      'Diana has her birthday coming up soon',
+      new Date(2020, 3, 14)
+    ),
+    new Quote(
+      4,
+      'Get Dog Food',
+      'Pupper likes expensive snacks',
+      new Date(2020, 3, 14)
+    ),
+    new Quote(5, 'Solve math homework', 'Damn Math', new Date(2020, 3, 14)),
+    new Quote(
+      6,
+      'Plot my world domination plan',
+      'Cause I am an evil overlord',
+      new Date(2020, 3, 14)
+    ),
   ];
 
-  toggleDetails(index){
+  toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
 
-  deleteQuote(isComplete, index){
+  deleteQuote(isComplete, index) {
     if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
-      if (toDelete){
-        this.quotes.splice(index,1)
+      let toDelete = confirm(
+        `Are you sure you want to delete ${this.quotes[index].name}?`
+      );
+      if (toDelete) {
+        this.quotes.splice(index, 1);
       }
     }
   }
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
