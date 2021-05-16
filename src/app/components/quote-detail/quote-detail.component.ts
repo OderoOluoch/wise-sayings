67ff1515d1cf316894b849f951394e6548a98c3c
OderoOluoch <oderoluoch@gmail.com>
2021-05-16 17:31:07 +0300

    Starting out on Detail

--- src/app/components/quote-detail/quote-detail.component.ts
+++ src/app/components/quote-detail/quote-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from '../../quote';
 
 @Component({
   selector: 'app-quote-detail',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailComponent implements OnInit {
 
+  @Input() quote:Quote;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,10 @@
 export class Quote {
-    id: number;
-    name: string;
-    description:string;
+  showDescription: boolean;
+  constructor(
+    public id: number,
+    public name: string,
+    public description: string
+  ) {
+    this.showDescription = false;
+  }
 }
