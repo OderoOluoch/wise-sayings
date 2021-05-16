b792d076751a833b204d1e9604c5ab9e1997ebaa
OderoOluoch <oderoluoch@gmail.com>
2021-05-16 20:56:35 +0300

    Our MVP is set and ready...

--- src/app/components/quote-form/quote-form.component.ts
+++ src/app/components/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 import { Quote } from '../../quote';
 
 @Component({
@@ -7,8 +7,13 @@ import { Quote } from '../../quote';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-  
+
   newQuote = new Quote(0,'','', new Date()) 
+  @Output() addQuote = new EventEmitter<Quote>()
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote)
+  }
 
   constructor() { }
 
--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -56,6 +56,13 @@ export class QuoteComponent implements OnInit {
     }
   }
 
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
