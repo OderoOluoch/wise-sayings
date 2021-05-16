6f00950fb7b04065c8031573be93d5bd523e30c4
OderoOluoch <oderoluoch@gmail.com>
2021-05-16 18:22:48 +0300

    Pipe date in place but inversed

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,7 +3,8 @@ export class Quote {
   constructor(
     public id: number,
     public name: string,
-    public description: string
+    public description: string,
+    public completeDate: Date
   ) {
     this.showDescription = false;
   }
